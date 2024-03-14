a8880960eb4eabbcc5b317f300f566956ef2bdf1
Keila Islas <kislas@bsd220.org>
2024-03-14 09:29:41 -0500

    Initial Commit

---
+++ API Calling/API_CallingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_CallingApp.swift
+//  API Calling
+//
+//  Created by Keila Islas on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct API_CallingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
